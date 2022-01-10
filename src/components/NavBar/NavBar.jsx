7b820273c5d1b5ab8d719cc61ebfbde169027394
Ivour <ivourek@gmail.com>
2022-01-10 20:47:58 +0100

    button working

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,50 @@
 import React from "react";
+import LandscapeIcon from "@mui/icons-material/Landscape";
+import AlarmIcon from "@mui/icons-material/Alarm";
+import styles from "./NavBar.module.css";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import { useState, useEffect } from "react";
 
 const NavBar = () => {
-  return <div></div>;
+  const [isClicked, setIsClicked] = useState(false);
+
+  const toggleHandler = () => {
+    setIsClicked((prev) => !prev);
+  };
+
+  return (
+    <nav className={styles.nav}>
+      <div className={styles.logoContainer}>
+        <LandscapeIcon className={styles.a} fontSize="large" color="success" />
+        <Typography variant="h6">Jumanji App</Typography>
+      </div>
+
+      <div>
+        <Button
+          variant={isClicked ? "outlined" : "contained"}
+          color="success"
+          onClick={toggleHandler}
+        >
+          Toggle List
+        </Button>
+        <Button variant="contained" color="success">
+          Toggle List
+        </Button>
+        <Button variant="contained" color="success">
+          Toggle List
+        </Button>
+        <IconButton
+          color="secondary"
+          aria-label="add an alarm"
+          href="https://www.seznam.cz"
+        >
+          <AlarmIcon />
+        </IconButton>
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
